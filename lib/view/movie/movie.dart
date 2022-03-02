eccc013d807e1aa6297680d436a23cb2e6603aab
mlatifr <mlatifraldi@gmail.com>
2022-03-02 20:11:57 +0700

    list movie template

--- lib/view/movie/movie.dart
+++ lib/view/movie/movie.dart
@@ -14,7 +14,52 @@ class _MovieListState extends State<MovieList> {
       appBar: AppBar(
         title: Center(child: Text('Pilar Movies')),
       ),
-      body: SafeArea(child: Text('movie')),
+      body: SafeArea(
+          child: Column(children: [
+        Text('Movie'),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [for (var i = 0; i < 10; i++) widgetPosterMovie()],
+          ),
+        ),
+        Text('tv'),
+        Row(
+          children: [
+            Text('tv'),
+            Text('tv'),
+            Text('tv'),
+            Text('tv'),
+          ],
+        ),
+      ])),
+    );
+  }
+}
+
+class widgetPosterMovie extends StatelessWidget {
+  const widgetPosterMovie({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: 150,
+        height: 250,
+        color: Colors.blue,
+        child: Column(children: [
+          Image.network(
+            'https://image.tmdb.org/t/p/w500/w2PMyoyLU22YvrGK3smVM9fW1jj.jpg',
+            width: 100,
+            height: 200,
+          ),
+          Text('Judul'),
+          Text('Rate'),
+        ]),
+      ),
     );
   }
 }
