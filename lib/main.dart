017dd80ec594d3cc5eaf21b9b28e15eb142cb4a8
mlatifr <mlatifraldi@gmail.com>
2022-03-02 23:10:37 +0700

    manifest allow internet + template cek connectiion

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pilar/providers/movie_provider.dart';
+import 'package:pilar/singletons/offlinepage.dart';
 import 'package:pilar/view/movie/home_page.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -8,11 +9,13 @@ void main() {
   runApp(const MyApp());
 }
 
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
    @override
   Widget build(BuildContext context) {
+    ConnectionStatus.getInstance().initialize(navigatorKey);
     return MultiProvider(
       providers:   [
         ChangeNotifierProvider(
---
+++ lib/view/movie/check_connection.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class OfflinePage extends StatefulWidget {
+  var title;
+  OfflinePage({Key? key,this.title}) : super(key: key);
+
+  @override
+  State<OfflinePage> createState() => _OfflinePageState();
+}
+
+class _OfflinePageState extends State<OfflinePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: widget.title,
+      ),
+      body: Center(child: Text('No Internet Connection')),
+    );
+  }
+}
