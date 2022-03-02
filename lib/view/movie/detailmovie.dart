e397ce700fb36ec4748eeaefadf82160dbd9394f
mlatifr <mlatifraldi@gmail.com>
2022-03-03 00:30:13 +0700

    re arrange detail movie

--- lib/view/movie/detailmovie.dart
+++ lib/view/movie/detailmovie.dart
@@ -20,6 +20,7 @@ class _DetailMovieState extends State<DetailMovie> {
           'https://image.tmdb.org/t/p/w500/1BIoJGKbXjdFDAqUEiA2VHqkK1Z.jpg',
           height: MediaQuery.of(context).size.height,
         ),
+        //detail movie widget
         Positioned(
             top: MediaQuery.of(context).size.height / 2,
             child: ClipRRect(
@@ -30,20 +31,49 @@ class _DetailMovieState extends State<DetailMovie> {
                   color: Colors.blue,
                   child: Column(
                     children: [
-                      const Text('Rating'),
-                      const Text('Judul'),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text('Rating'),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text('Judul'),
+                      ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
+                        children: [
                           Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text('Genre'),
+                            padding: const EdgeInsets.all(20.0),
+                            child: Row(
+                              children: const [
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text('Genre'),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text('Genre'),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text('Genre'),
+                                ),
+                              ],
+                            ),
                           ),
-                          Padding(
-                            padding: EdgeInsets.all(8.0),
+                          const Padding(
+                            padding: EdgeInsets.all(20.0),
                             child: Text('Durasi'),
                           ),
                         ],
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Text(
+                          'Sinopsis: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrs standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has sur ',
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                        ),
                       ),
                     ],
                   )),
